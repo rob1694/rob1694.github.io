33d25d0e938cab5579ce6773dea0a7b5ce7b7d85
rob1694 <robertb1694@gmail.com>
2021-03-28 02:00:58 -0400

    completed

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const steps = [
-    { stepNumber: 'Step 1', description: 'Come up with an idea that interests you' },
+    { stepNumber: 'Step 1', description: 'Come up with an topic that interests you' },
     { stepNumber: 'Step 2', description: 'Gain knowledge, insight, and inspiration to gain traction' },
     { stepNumber: 'Step 3', description: 'Create an approach to accomplish that idea' },
     { stepNumber: 'Step 4', description: 'Think about how your going to take that approach' },
-    { stepNumber: 'Step 5', description: 'Create the basics' },
+    { stepNumber: 'Step 5', description: 'Create the basics of your website' },
     { stepNumber: 'Step 6', description: 'Dive into complexity' },
     { stepNumber: 'Step 7', description: 'Touch up and conclude' },
 
